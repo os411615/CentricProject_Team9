4e6fcfb351a700ec79877f9e0df4653ca79de347
Cameron Nagy <cn894815@ohio.edu>
2020-04-18 21:53:31 -0400

    New Models (cn)

---
+++ Models/EmployeeDetails.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CentricProject_Team9.Models
+{
+    public class EmployeeDetails
+    {
+        [Key]
+        [Required]
+        public Guid employeeID { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string fullName
+        {
+            get { return lastName + ", " + firstName; }
+        }
+
+        // Personal Info
+        [Required(ErrorMessage = "Please include your @centricconsulting.com address.")]
+        [EmailAddress]
+        [Display(Name = "Employee Email")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Please include your first name.")]
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+
+        [Required(ErrorMessage = "Please include your last name.")]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        [Required(ErrorMessage = "Please include your office phone number.")]
+        [Display(Name = "Office Phone")]
+        [Phone]
+        public string phoneNumber { get; set; }
+
+
+        // location dd
+        [Required]
+        [Display(Name = "Office Location")]
+        public OfficeLocation officeLocation { get; set; }
+        public enum OfficeLocation
+        {
+            Boston,
+            Charlotte,
+            Chicago,
+            Cincinnati,
+            Cleveland,
+            Columbus,
+            India,
+            Indianapolis,
+            Louisville,
+            Miami,
+            Seattle,
+            [Display(Name = "St. Louis")]
+            StLouis,
+            Tampa
+        }
+
+        // department dd
+        [Required]
+        [Display(Name = "Department")]
+        public Department department { get; set; }
+        public enum Department
+        {
+            Accounting,
+            [Display(Name = "Business Consulting")]
+            BusinessConsulting,
+            [Display(Name = "Digital Consulting")]
+            DigitalConsulting,
+            Finance,
+            [Display(Name = "Human Resources")]
+            HumanResources,
+            [Display(Name = "Technology Services")]
+            TechnologyServices
+        }
+
+
+        // Position Details
+        [Required(ErrorMessage = "Please include your current position title.")]
+        [Display(Name = "Current Position")]
+        public string position { get; set; }
+
+        [Required(ErrorMessage = "Please include your current manager.")]
+        [Display(Name = "Manager")]
+        public string manager { get; set; }
+
+        [Required(ErrorMessage = "Please include your initial hire date.")]
+        [Display(Name = "Hire Date")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime hireDate { get; set; }
+
+
+        // Link to recognitions table
+        public ICollection<Recognitions> Recognitions { get; set; }
+    }
+}
---
+++ Models/Recognitions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace CentricProject_Team9.Models
+{
+    public class Recognitions
+    {
+        [Key]
+        public int recognitionID { get; set; }
+
+        [Display(Name = "Recognizer")]
+
+        public Guid From { get; set; }
+        [ForeignKey("From")]
+        public virtual EmployeeDetails EmployeeRecognizer { get; set; }
+
+
+        // Employee being recognized for their core values
+
+        [Display(Name = "Recipient")]
+        public Guid employeeID { get; set; }
+        [ForeignKey("employeeID")]
+        public virtual EmployeeDetails EmployeeRecognized { get; set; }
+
+
+        //Core Values ddl
+        [Display(Name = "Core Value")]
+        public CoreValue award { get; set; }
+        public enum CoreValue
+        {
+            Excellence = 1,
+            Integrity = 2,
+            Stewardship = 3,
+            Culture = 4,
+            Passion = 5,
+            Innovate = 6,
+            Lifestyle = 7
+        }
+
+        [Display(Name = "Description")]
+        public string description { get; set; }
+
+        [Display(Name = "Date Given")]
+        public DateTime recognizationDate { get; set; }
+    }
+}
